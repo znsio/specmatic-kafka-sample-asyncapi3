2471164a7c6190e59c1bec7a9119d61430521562
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2025-05-22 09:00:32 +0530

    test: modify the contract tests to run using test containers

--- src/test/kotlin/com/example/order/ContractTest.kt
+++ src/test/kotlin/com/example/order/ContractTest.kt
@@ -1,34 +1,41 @@
 package com.example.order
 
-import io.specmatic.kafka.KafkaMock
-import io.specmatic.kafka.SpecmaticKafkaContractTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ConfigurableApplicationContext
+import org.testcontainers.containers.BindMode
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 
-private const val IN_MEMORY_BROKER_HOST = "localhost"
-private const val IN_MEMORY_BROKER_PORT = 9092
-
-class ContractTest : SpecmaticKafkaContractTest {
+@Testcontainers
+class ContractTest {
     companion object {
         private lateinit var context: ConfigurableApplicationContext
-        private lateinit var kafkaMock: KafkaMock
 
         @JvmStatic
         @BeforeAll
         fun setup() {
             System.setProperty("OVERLAY_FILE", "src/test/resources/spec_overlay.yaml")
             System.setProperty("CONSUMER_GROUP_ID", "order-consumer-group-id")
-            kafkaMock = KafkaMock.startInMemoryBroker(IN_MEMORY_BROKER_HOST, IN_MEMORY_BROKER_PORT)
+            zookeeper.start()
+            kafkaBroker.start()
             startApplication()
         }
 
         @JvmStatic
         @AfterAll
         fun tearDown() {
+            zookeeper.stop()
+            kafkaBroker.stop()
             stopApplication()
-            kafkaMock.stop()
         }
 
         private fun startApplication() {
@@ -39,5 +46,65 @@ class ContractTest : SpecmaticKafkaContractTest {
         private fun stopApplication() {
             context.stop()
         }
+
+        private val network = Network.newNetwork()
+
+        private val zookeeper = GenericContainer(DockerImageName.parse("confluentinc/cp-zookeeper:7.2.1"))
+            .withEnv("ZOOKEEPER_CLIENT_PORT", "2181")
+            .withNetwork(network)
+            .withNetworkAliases("zookeeper")
+            .withExposedPorts(2181)
+            .waitingFor(HostPortWaitStrategy().forPorts(2181))
+
+        private val kafkaBroker = GenericContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"))
+            .withNetwork(network)
+            .withExposedPorts(9092)
+            .withEnv("KAFKA_BROKER_ID", "1")
+            .withEnv("KAFKA_ZOOKEEPER_CONNECT", "zookeeper:2181")
+            .withEnv("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://localhost:9092")
+            .withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9092")
+            .withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
+            .withNetworkAliases("kafka")
+            .withCreateContainerCmdModifier { cmd ->
+                cmd.hostConfig?.withPortBindings(
+                    com.github.dockerjava.api.model.PortBinding.parse("9092:9092")
+                )
+            }
+            .waitingFor(HostPortWaitStrategy().forPorts(9092))
+    }
+
+    private val testContainer: GenericContainer<*> = GenericContainer("znsio/specmatic-kafka")
+        .withCommand(
+            "test",
+            "--host=localhost",
+            "--port=9092",
+            "--overlay=spec_overlay.yaml"
+            // Set the external examples directory if there are no inline examples in the spec
+            // "--examples=examples"
+        )
+        .withNetworkMode("host")
+        .withFileSystemBind(
+            "./src/test/resources/specmatic.yaml",
+            "/usr/src/app/specmatic.yaml",
+            BindMode.READ_ONLY
+        )
+        .withFileSystemBind(
+            "./src/test/resources/spec_overlay.yaml",
+            "/usr/src/app/spec_overlay.yaml",
+            BindMode.READ_ONLY
+        )
+        .withFileSystemBind(
+            "./build/reports/",
+            "/usr/src/app/build/reports/",
+            BindMode.READ_WRITE
+        )
+        .waitingFor(Wait.forLogMessage(".*Tests run:.*", 1))
+        .withLogConsumer { print(it.utf8String) }
+
+    @Test
+    fun contractTests() {
+        testContainer.start()
+        val hasSucceeded = testContainer.logs.contains("Result: FAILED").not()
+        assertThat(hasSucceeded).isTrue()
     }
 }
